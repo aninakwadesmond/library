6492b2ff732bfd85c32f7661ef2bac5ef10094f0
aninakwadesmond <aninakwadesmond3@gmail.com>
2026-04-02 05:14:57 +0000

    change

--- frontend/src/Axios/api.jsx
+++ frontend/src/Axios/api.jsx
@@ -17,7 +17,8 @@ const apiAuthor = axios.create({
 });
 
 const server = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://library-oz1u.onrender.com",
+  //http://localhost:5000
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
